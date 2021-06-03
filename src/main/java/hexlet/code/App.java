a3bee2e85c6fbbf39f235305c999f7cd10f51909
Dinara.Aleskarova <aleskarova@gmail.com>
2021-06-03 11:56:20 +0400

    Added format option and filepath1, filepath2 parameters

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,10 +2,22 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
          description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format",
+            defaultValue = "stylish", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+    private String format;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to the first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to the second file")
+    private String filepath2;
 
     @Override
     public void run() {
